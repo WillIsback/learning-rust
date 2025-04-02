0412ab429a5dc809bcf5f85c7646ad47b9f916f2
Will <wderue-cli@PO-5CD028C55B.ad.in.region-academique-paca.fr>
2025-04-02 09:48:11 +0200

    init commit

---
+++ src/main.rs
@@ -0,0 +1,32 @@
+// function to generate login and password
+fn generate_login_and_password(firstname : str,
+                                        lastname : str,
+                                        email: str,
+                                        burge: str,
+                                        si: str) -> Vec<<&str>> : {
+        let mut _firstname = firstname;
+        let mut _lastname = lastname;
+        let mut _email = email;
+        let mut _burge = burge;
+        let mut _si = si;
+        println!("this is a placeholder for the generate_login_and_password function");
+
+}
+
+
+
+
+// function to generate a putty user login
+fn generate_user(){
+    println!("placeholder for the generate user function");
+    generate_login_and_password(firstname="william",lastname="derue",email="william.derue@ac-aix-marseille.fr",burge="PID***",si="AGAPE")
+}
+
+
+
+
+
+fn main() {
+    println!("Hello, world!");
+    generate_user();
+}
